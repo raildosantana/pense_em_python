cf03be9c93fb1852612fd17709503b4cfbe8bcd7
Raildo Santana <raildo.santana@live.com>
2020-07-24 11:12:43 -0300

    Terminando primeira unidade

---
+++ 1. A jornada do programa/exercicio12.py
@@ -0,0 +1,11 @@
+
+
+print("Quantos segundos há em 42 minutos e 42 segundos? %d s"%(42 * 60 + 42))
+
+print("Quantas milhas há em 10 quilômetros? %.2f milhas"%(10 * 1.61))
+
+print("Se você correr 10 quilômetros em 42 minutos e 42 segundos, qual é o seu passo médio ? Qual é a sua velocidade média em milhas por hora?")
+milhas = 10 * 1.61
+horas = (42 * 60 + 42) / 3600
+
+print("%.2f m/s e %.2f mph"%(10000/(42 * 60 + 42),milhas/horas))
