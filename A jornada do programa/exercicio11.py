ecc55de3d6a1430caae1c3cccce1ba0a581e2598
Raildo Santana <raildo.santana@live.com>
2020-07-24 09:33:25 -0300

    Primeira Unidade Exercicio 1.1

---
+++ A jornada do programa/exercicio11.py
@@ -0,0 +1,18 @@
+# Em uma instrução print, o que acontece se você omitir um dos parênteses ou ambos?
+# Em Python 3 é necessario o uso dos parêntese na função print(), caso contrario ocorrera
+# um erro de sintaxe : SyntaxError: Missing parentheses in call to 'print'.
+
+# Se estiver tentando imprimir uma string, o que acontece se omitir uma das aspas ou ambas?
+# Ocorre um erro, caso tente imprimir sem uma ou ambas as aspas
+# SyntaxError: EOL while scanning string literal
+
+#Você pode usar um sinal de menos para fazer um número negativo como -2. 
+#O que acontece se puser um sinal de mais antes de um número? E se escrever assim: 2++2?
+#É processado naturalmente 2++2 = 4 , -2+2 = 0
+
+#Na notação matemática, zeros à esquerda são aceitáveis, como em 02. O que acontece se você tentar usar isso no Python?
+#Não é aceitavel , não é tipo numerico valido
+
+#O que acontece se você tiver dois valores sem nenhum operador entre eles?
+# Ocorre um erro de sintaxe
+# SyntaxError: invalid syntax
